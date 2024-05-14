e1af51635f376806a35ffd4493e2da93c9885720
drsanti <santi.inc.kmutt@gmail.com>
2024-05-14 08:18:57 +0700

    updates

--- examples/my-next-app/app/page.tsx
+++ examples/my-next-app/app/page.tsx
@@ -1,39 +1,50 @@
-"use client";
-import { Progress } from "@/components/ui/progress";
-import { QNetBrowserSocket } from "qnetbrowsersocket";
-import { useEffect, useState } from "react";
-export default function Home() {
-  const [time, setTime] = useState<string>("");
-  const [counter, setCounter] = useState<number>(0);
-  const [highRes, setHighRes] = useState<number>(0);
+// "use client";
+// import { useEffect, useState } from "react";
+// const boxCls = `text-white w-[100px] px-4 py-2 rounded text-center m-2`;
+// const boxRed = `bg-red-500 ${boxCls}`;
+// const boxGreen = `bg-green-500 ${boxCls}`;
+// export default function Home() {
+//   const N = 200;
 
-  useEffect(() => {
-    const qnet = new QNetBrowserSocket({
-      onMessage: (msg: string) => {
-        /** console.log(msg) to check how many things sent from server */
-        const json = JSON.parse(msg);
-        setTime(() => json.systemTime);
-        setCounter(() => json.counter);
-        setHighRes(() => json.highResolutionTime);
-      },
-    });
-    qnet.start().then((s) => {
-      console.log(s);
-    });
-    return () => {
-      qnet.stop();
-    };
-  }, []);
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-500">
+//       <div className="flex flex-col bg-black w-1/2 p-4 rounded-xl">
+//         <h1 className="text-white text-4xl">Control Panel</h1>
+//         <div className="flex flex-row justify-center">
+//           <button className="bg-green-600 text-white p-2 rounded-md py-4 m-4 flex-grow-[2]">
+//             Start
+//           </button>
+//           <button className="bg-red-600 text-white p-2 rounded-md py-4 m-4 flex-grow-[1]">
+//             Stop
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-  return (
-    <div>
-      <p>Time: {time}</p>
-      <p>Counter:{counter}</p>
-      <p>{highRes}</p>
-      <button className="bg-red-500 text-white w-[300px] px-4 py-2 rounded">
-        {highRes}
-      </button>
-      <Progress value={counter % 100} />
-    </div>
-  );
-}
+// "use client";
+// import { useEffect, useState } from "react";
+// const boxCls = `text-white w-[100px] px-4 py-2 rounded text-center m-2`;
+// const boxRed = `bg-red-500 ${boxCls}`;
+// const boxGreen = `bg-green-500 ${boxCls}`;
+// export default function Home() {
+//   const N = 200;
+//   const getSensorValues = () => {
+//     const values = [];
+//     for (let i = 0; i < N; i++) {
+//       // values.push(Math.random() * 100);
+//       values.push(i);
+//     }
+//     return values;
+//   };
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-500">
+//       <div className="flex flex-wrap">
+//         {getSensorValues().map((value, index) => (
+//           <div className={value > N / 2 ? boxRed : boxGreen}>{value}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
